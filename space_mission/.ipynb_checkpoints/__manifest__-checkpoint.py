da75a3f3191cbc5c1a4315ca8db1304495a27339
Tepava <mehdi@pacific-erp.com>
2022-02-07 22:06:12 +0000

    module initialisation

---
+++ space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Space Mission (MT)",
+    'summary': """
+        Space mission for ODoo training (MT)""",
+    'description': """
+        Application to organize the logistics including spaceship and space crew
+    """,
+    'author': "Mehdi Tepava",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base','sale_management'],
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/settings.xml',
+        #'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+    'license': 'LGPL-3',
+}
